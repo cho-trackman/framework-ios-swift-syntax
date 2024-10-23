d48975e3b09bad63b244844fd98642f83a81d44f
Christoffer Hoel <cho@trackman.com>
2024-10-23 13:31:46 +0200

    D'oh

--- .github/comment-unsupported.py
+++ .github/comment-unsupported.py
@@ -1,14 +1,19 @@
 import re
 import sys
+import difflib
 
 filename = sys.argv[1]
 
 content = None
+new_content = None
 with open(filename, 'r' ) as f:
     content = f.read()
+    new_content = re.sub(r'((^@available\([^\)]*?\)\s)?^extension Swift\.Float16[^}]*?})', r'/*\n\1\n*/', content, flags = re.M)
 
-content = re.sub(r'(!/\*)((^@available\([^\)]*?\)\s)?^extension Swift\.Float16[^}]*?})', r'/*\n\1\n*/', content, flags = re.M)
-#print(content)
+diff=difflib.unified_diff(content, new_content)
+print(''.join(diff))
 
 with open(filename, 'w' ) as f:
-    f.write(content)
+    f.write(new_content)
+
+print("Done!")
